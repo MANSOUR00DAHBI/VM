ce205186b29e798bef78ae9eafec18d958e4978d
kounax <121632169+MANSOUR00DAHBI@users.noreply.github.com>
2025-10-23 21:21:26 +0000

    add libarary

--- include/included.h
+++ include/included.h
@@ -4,4 +4,15 @@
 #define INC_H
 
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <windows.h>
+#include <unistd.h>
+
 #endif 
