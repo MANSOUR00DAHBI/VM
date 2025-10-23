bd6448fe65799256f756c7d3b385a66efaab50ad
kounax <121632169+MANSOUR00DAHBI@users.noreply.github.com>
2025-10-23 21:24:52 +0000

    .

--- include/vm.h
+++ include/vm.h
@@ -4,12 +4,13 @@
 #define  VM_H
 #include <interface.h>
 #define CAPACITY_STACK 1024
+
 typedef int64_t Word ;
 typedef struct {
   Word Stack[CAPACITY_STACK];
   Word instruction_pointer;
   int halt ;
 
-}
+}vm;
 
 #endif
